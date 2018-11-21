8821f632dc6572e3328a027cc9ebc561f735027e
Jordi Puiggené <jpuiggene@naralabs.com>
2018-11-21 12:12:53 +0100

    Pinned version of senaite.core

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'senaite.core',
-        "senaite.core==1.2.9",
+        "senaite.core>=1.2.9",
+        "senaite.core<1.3",
         'archetypes.schemaextender',
     ],
     extras_require={
